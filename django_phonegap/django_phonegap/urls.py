96a12cfc405613aee57249710ae9d699e1732fb4
JooeunAhn <jflow@hanyang.ac.kr>
2016-09-09 11:21:54 +0800

    testing page

--- django_phonegap/django_phonegap/urls.py
+++ django_phonegap/django_phonegap/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic  import TemplateView
 from tastypie.api import Api
 from .api import LoginResource
 from blog.views import home
 from blog.api import PostingResource
+
+
+
 v1_api = Api(api_name="v1")
 v1_api.register(LoginResource())
 v1_api.register(PostingResource())
 
 
-
 urlpatterns = patterns('',
+    url(r'testing/', TemplateView.as_view(template_name="ajax_testing.html")),
     url(r'^api/',include(v1_api.urls)),
     url(r'^$', home, name="home"),
     url(r'^admin/', include(admin.site.urls)),
